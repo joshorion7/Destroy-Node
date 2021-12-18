cddd8433f4f7f6d7f620f1fb195b6426f0ebcbae
Josh Butler <joshorion7@gmail.com>
2021-12-18 12:43:01 -0500

    organized JS

--- cli.js
+++ cli.js
@@ -4,20 +4,6 @@ import chalk from 'chalk';
 import fs from 'fs-extra';
 
 // Removes specified directory and it's contents
-// Using - fs from 'fs'
-// fs.rmdir("./node_modules",
-//     { recursive: true, force: true }, (err) => {
-
-//         if (err) {
-//             return console.log(chalk.bgRed.bold("You messed up ya dummy, so read this ---> ", err));
-//         }
-
-//         console.log(chalk.green("You deleted those gosh darn node thingys. Congrats, you're a genuis."));
-//     }
-// );
-
-//----------------------------------------------
-// Removes specified directory and it's contents
 // Using - fs-extra
 const folder = './node_modules';
 
@@ -30,8 +16,21 @@ fs.remove(folder, err => {
     }
 
 });
+//---------------------------------------------
+//---------------------------------------------
+// Removes specified directory and it's contents
+// Using - fs from 'fs'
+// fs.rmdir("./node_modules",
+//     { recursive: true, force: true }, (err) => {
 
+//         if (err) {
+//             return console.log(chalk.bgRed.bold("You messed up ya dummy, so read this ---> ", err));
+//         }
 
+//         console.log(chalk.green("You deleted those gosh darn node thingys. Congrats, you're a genuis."));
+//     }
+// );
+//---------------------------------------------
 //---------------------------------------------
 // Attempt - Creating function to loop through all directories with the name 'node_modules' and delete
 // let delDirectories = () => {
@@ -53,3 +52,5 @@ fs.remove(folder, err => {
 
 // };
 // delDirectories();
+//---------------------------------------------
+//---------------------------------------------
